d593f65ba63462af5606cf16428d66d2ac46193a
BonnW <pwonghansa@gmail.com>
2017-10-12 17:27:23 -0500

    Tree Add rough draft written

--- src/tree.js
+++ src/tree.js
@@ -5,9 +5,40 @@ class Tree {
     this.value = value;
     this.children = [];
   }
-  // Adds a new Tree node with the input value to the current Tree node 
+  // Adds a new Tree node with the input value to the current Tree node
   addChild(value) {
+    const node = {
+      value,
+      left: null,
+      right: null,
+    };
+    let current;
 
+    if (this.root === null) {
+      this.root = node;
+    } else {
+      current = this.root;
+
+      while(true) {
+        if(value < current.value) {
+          if(currnet.left === null) {
+          current.left = node;
+          break;
+        } else {
+          current = current.left;
+        };
+      } else if (value > current.value) {
+        if(current.right === null) {
+          current.right = node;
+          break;
+        } else {
+          current = current.right;
+        }
+      } else {
+        break;
+        }
+      }
+    }
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
